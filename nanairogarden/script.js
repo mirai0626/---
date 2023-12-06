c5064470e37392817605754f69e23ae628520c25
mirai0626 <0516noi.s@gmail.com>
2023-12-06 12:04:44 +0900

    進級展

--- nanairogarden/script.js
+++ nanairogarden/script.js
@@ -8,3 +8,17 @@ hamburger.addEventListener('click', function () {
 
   console.log('hi');
 });
+
+// glowAnimeにglowというクラス名を付ける定義
+function GlowAnimeControl() {
+  $('.glowAnime').each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass('glow');
+    } else {
+      $(this).removeClass('glow');
+    }
+  });
+}
